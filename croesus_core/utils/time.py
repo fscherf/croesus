76e34a37691329b58ba7fb55b6cf0cdb1ae84a88
Florian Scherf <f.scherf@pengutronix.de>
2016-10-30 15:55:11 +0100

    utils: add time tools
    Signed-off-by: Florian Scherf <f.scherf@pengutronix.de>

---
+++ croesus_core/utils/time.py
@@ -0,0 +1,14 @@
+MONTH_NAMES = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+]
