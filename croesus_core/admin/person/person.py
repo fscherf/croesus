e6ae10f0121dbd4f17836c9954b594ba82f74fb7
Florian Scherf <f.scherf@pengutronix.de>
2016-10-30 19:22:36 +0100

    admin: Person: add inlines
    Signed-off-by: Florian Scherf <f.scherf@pengutronix.de>

--- croesus_core/admin/person/person.py
+++ croesus_core/admin/person/person.py
@@ -1,10 +1,26 @@
 from django.contrib import admin
 
+from ...models import MembershipFeeAgreement, PersonInactiveRule
 from ...forms import PersonForm
+
+
+class MembershipFeeAgreementInline(admin.TabularInline):
+    model = MembershipFeeAgreement
+    extra = 1
+
+
+class PersonInactiveRuleInline(admin.TabularInline):
+    model = PersonInactiveRule
+    extra = 1
 
 
 class PersonAdmin(admin.ModelAdmin):
     form = PersonForm
+
+    inlines = [
+        MembershipFeeAgreementInline,
+        PersonInactiveRuleInline,
+    ]
 
     list_display = (
         'name',
