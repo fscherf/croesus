61b093dc8067fd0358c7ada4e966e6d8672e88f7
Florian Scherf <f.scherf@pengutronix.de>
2016-11-11 10:41:45 +0100

    packaging: setup: add classifiers
    Signed-off-by: Florian Scherf <f.scherf@pengutronix.de>

--- setup.py
+++ setup.py
@@ -19,4 +19,19 @@ setup(
         'PTable',
         'pyyaml',
     ],
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Framework :: Django :: 1.8',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Office/Business :: Financial :: Accounting',
+        'Intended Audience :: Financial and Insurance Industry',
+    ]
 )
